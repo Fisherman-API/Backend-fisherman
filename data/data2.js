1f08e93627e1ff4baaae66b0311d45239488e70c
sirdan <stanford303@gmail.com>
2022-08-29 14:41:54 -0700

    controller for regs built

--- data/data2.js
+++ data/data2.js
@@ -70,6 +70,77 @@ const regs = [
     'rules': '5 per day to Sep 5, 4 per day after Sep 6',
     'seasons': 'All Year'
   },
+  {
+    'id': '11',
+    'name': 'Stripped Bass',
+    'region': 'Columbia zone',
+    'rules': 'No Limit, Any Size',
+    'seasons': 'All Year'
+
+  },
+  {
+    'id': '12',
+    'name': 'Blue Gill',
+    'region': 'Columbia zone',
+    'rules': 'No Limit, Any Size',
+    'seasons': 'All Year'
+  },
+  {
+    'id': '13',
+    'name': 'Cat Fish',
+    'region': 'Columbia zone',
+    'rules': 'No Limit, Any Size',
+    'seasons': 'All Year'
+  },
+  {
+    'id': '14',
+    'name': 'crappie',
+    'region': 'Columbia zone',
+    'rules': 'No Limit, Any Size',
+    'seasons': 'All Year'
+  },
+  {
+    'id': '15',
+    'name': 'Sun Fish',
+    'region': 'Columbia zone',
+    'rules': 'No Limit, Any Size',
+    'seasons': 'All Year'
+  },
+  {
+    'id': '16',
+    'name': 'WallEye',
+    'region': 'Columbia zone',
+    'rules': 'No Limit, Any Size',
+    'seasons': 'All Year'
+  },
+  {
+    'id': '17',
+    'name': 'Perch',
+    'region': 'Columbia zone',
+    'rules': 'No Limit, Any Size',
+    'seasons': 'All Year'
+  },
+  {
+    'id': '18',
+    'name': 'Tuna',
+    'region': 'Marine zone',
+    'rules': 'Keep any size, bag limit 25,',
+    'seasons': 'All Year'
+  },
+  {
+    'id': '19',
+    'name': 'Flat Fish',
+    'region': 'Marine zone',
+    'rules': '25 per year',
+    'seasons': 'All Year'
+  },
+  {
+    'id': '20',
+    'name': 'SurfPerch',
+    'region': 'Marine zone',
+    'rules': '15 per day',
+    'seasons': 'All Year'
+  },
 
 ];
   
