43230ce6faa4ae2ac40cd657e0fbdba973d25063
Pedro <pedro.chn01@gmail.com>
2022-08-29 17:02:28 -0700

    fixing conflicts between pulls

--- __tests__/regulations.test.js
+++ __tests__/regulations.test.js
@@ -3,15 +3,6 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-
-
-
-
-
-
-
-
-
 describe('user routes', () => {
   beforeEach(() => {
     return setup(pool);
