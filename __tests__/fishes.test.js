8d27db9f4acd5a4d1017ea99c7b9b00838b39e47
Sean Moriarty <spmoriarty1@gmail.com>
2022-08-30 16:39:02 -0700

    eslint stuff

--- __tests__/fishes.test.js
+++ __tests__/fishes.test.js
@@ -2,7 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const app = require('../lib/app');
 const request = require('supertest');
-const UserService = require('../lib/services/UserService');
+
 
 const mockUser = {
   username: 'mock',
