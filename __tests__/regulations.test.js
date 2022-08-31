cec5d0652886dc4fb88805d0079312ab2c2dff38
Sean Moriarty <spmoriarty1@gmail.com>
2022-08-31 12:35:03 -0700

    working update with author/cate protected routes for regulations

--- __tests__/regulations.test.js
+++ __tests__/regulations.test.js
@@ -2,6 +2,30 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
+
+
+const mockUser = {
+  username: 'mock',
+  password: '123456',
+  email: 'test@example.com',
+};
+
+const newReg = {
+  name: 'Snapper',
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+  const agent = request.agent(app);
+  const user = await UserService.create({ ...mockUser, ...userProps });
+  const { email } = user;
+  await agent.post('/api/v1/users/session').send({ email, password });
+  return [agent, user];
+};
+
+
+
 
 describe('user routes', () => {
   beforeEach(() => {
@@ -21,14 +45,22 @@ describe('user routes', () => {
   });
 
   it('/:id should get individual regulation', async () => {
-    const resp = await request(app).get('/api/v1/regulations/1');
+    const [agent] = await registerAndLogin();
+    const resp = await agent.get('/api/v1/regulations/1');
     expect(resp.body).toEqual({
-      id: "1",
+      id: '1',
       name: expect.any(String),
       detail: expect.any(String),
     });
   });
 
+  it('/:id should update regulations', async () => {
+    const [agent] = await registerAndLogin();
+    const updatedResp = await agent.put('/api/v1/regulations/1').send(newReg);
+    console.log(updatedResp.body);
+    expect(updatedResp.status).toBe(200);
+    expect(updatedResp.body.name).toBe('Snapper');
+  });
 
 
   
