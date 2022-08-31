5cb8a060168f2c8f1c882a364751394ff436a857
Aaron Enyetu <aaronenyetu@gmail.com>
2022-08-31 10:16:39 -0700

    Test passing to return a list of all fishes

--- __tests__/fishes.test.js
+++ __tests__/fishes.test.js
@@ -3,7 +3,6 @@ const setup = require('../data/setup');
 const app = require('../lib/app');
 const request = require('supertest');
 
-
 const mockUser = {
   username: 'mock',
   password: '123456',
@@ -13,12 +12,13 @@ const mockUser = {
 const registerAndLogin = async (userProps = {}) => {
   // const password = userProps.password ?? mockUser.password;
   const agent = request.agent(app);
-  const user = await agent.post('/api/v1/users').send({ ...mockUser, ...userProps });
+  const user = await agent
+    .post('/api/v1/users')
+    .send({ ...mockUser, ...userProps });
   // const { email } = user;
   // await agent.post('/api/v1/users').send({ email, password });
   return [agent, user];
 };
-
 
 describe('user routes', () => {
   beforeEach(() => {
@@ -28,15 +28,27 @@ describe('user routes', () => {
   afterAll(() => {
     pool.end();
   });
-  it.only(' get location and bycatch data', async () => {
-    const [agent] = await registerAndLogin();
-    const res = await agent.get('/api/v1/fishes/red-snapper/location');
 
-    expect(res.body).toEqual({
-      location:
-        '<ul>\n<li>Red snapper are generally found at 30 to 620 feet deep in the Gulf of Mexico and along the eastern coasts of North America, Central America, and northern South America.</li>\n<li>They are rare north of the Carolinas.</li>\n</ul>\n',
-      bycatch:
-        'Regulations require modified fishing gear to reduce bycatch. Release techniques improve the chance of survival of unintentionally caught fish.',
+  it.only('#GET /fishes should return a list of all fishes', async () => {
+    const response = await request(app).get('/api/v1/fishes');
+
+    expect(response.status).toBe(200);
+    expect(response.body[0]).toEqual({
+      id: expect.any(String),
+      name: expect.any(String),
+      detail: expect.any(String),
     });
   });
 });
+
+//   it.only(' get location and bycatch data', async () => {
+//     const [agent] = await registerAndLogin();
+//     const res = await agent.get('/api/v1/fishes/red-snapper/location');
+
+//     expect(res.body).toEqual({
+//       location:
+//         '<ul>\n<li>Red snapper are generally found at 30 to 620 feet deep in the Gulf of Mexico and along the eastern coasts of North America, Central America, and northern South America.</li>\n<li>They are rare north of the Carolinas.</li>\n</ul>\n',
+//       bycatch:
+//         'Regulations require modified fishing gear to reduce bycatch. Release techniques improve the chance of survival of unintentionally caught fish.',
+//     });
+//   });
